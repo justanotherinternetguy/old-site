4763c585d6f1c1a60c8e7885443c830f69fb43aa
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-08-01 18:04:34 -0400

    e
    e

--- toggle.js
+++ toggle.js
@@ -3,17 +3,15 @@ const descNormal = document.querySelector(".desc-normal");
 const togBtn = document.querySelector("#togBtn");
 
 window.onload = function WindowLoad(event) {
-    togBtn.checked = false;
-}
-
+  togBtn.checked = false;
+};
 
 togBtn.addEventListener("click", function () {
-    if (togBtn.checked == true) {
-        descNerd.classList.add("hide-text");
-        descNormal.classList.remove("hide-text");
-
-    } else {
-        descNerd.classList.remove("hide-text");
-        descNormal.classList.add("hide-text");
-    }
-})
+  if (togBtn.checked == true) {
+    descNerd.classList.add("hide-text");
+    descNormal.classList.remove("hide-text");
+  } else {
+    descNerd.classList.remove("hide-text");
+    descNormal.classList.add("hide-text");
+  }
+});
